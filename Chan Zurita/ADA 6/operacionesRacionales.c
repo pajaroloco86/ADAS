78c0448013b31fc5cb791450e35310c8738dbed0
Mario J. Chan Zurita <56327994+MarioJChanZurita@users.noreply.github.com>
2020-04-17 14:11:33 -0500

    Acomodo de ejercicios

--- Chan Zurita/ADA 6/operacionesRacionales.c
+++ Chan Zurita/ADA 6/operacionesRacionales.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-struct racional{
+typedef struct Racional{
 	int numerador;
 	int denominador;
-};
+} racional;
 
 void leerRacional(racional *r1);
 racional sumarRacionales(racional r1, racional r2);
 racional restarRacionales(racional r1, racional r2);
 racional multiplicarRacionales(racional r1, racional r2);
 racional dividirRacionales(racional r1, racional r2);
-void simplificarRacional(racional r3);
+void simplificarRacional(racional *r3);
 void imprimirRacional(racional r3);
 
 int main(){
 	
-	racional r1, r2, r3;
+	racional r1, r2;
 		
 	leerRacional(&r1);
 	leerRacional(&r2);
@@ -26,43 +26,50 @@ int main(){
 	multiplicarRacionales(r1,r2);
 	dividirRacionales(r1,r2);
 	
+	printf("Suma: \n");
 	imprimirRacional(sumarRacionales(r1,r2));
+	printf("Resta: \n");
 	imprimirRacional(restarRacionales(r1,r2));
+	printf("Multiplicación: \n");
 	imprimirRacional(multiplicarRacionales(r1,r2));
+	printf("División: \n");
 	imprimirRacional(dividirRacionales(r1,r2));
 	
 	return 0;
 }
 
-void leerRacional(racional r1){
+void leerRacional(racional *r1){
 	do{
-		printf("Primer Racional Numerador: ");
-		scanf("%d", &r1.numerador);
-		printf("Primer Racional Denominador: ");
-		scanf("%d", &r1.denominador);
-	}while(r1.denominador == 0);
+		printf("Racional Numerador: ");
+		scanf("%d", &r1->numerador);
+		printf("Racional Denominador: ");
+		scanf("%d", &r1->denominador);
+	}while(r1->denominador == 0 || r1->numerador == 0);
 }
-
 
 
 racional sumarRacionales(racional r1, racional r2){
 	racional r3;
 	r3.numerador = (r1.numerador * r2.denominador) + (r1.denominador * r2.numerador);
 	r3.denominador = (r1.denominador * r2.denominador);
+	simplificarRacional(&r3);
 	return r3; 
 }
 
-racional restarRacional(racional r1, racional r2){
+
+racional restarRacionales(racional r1, racional r2){
 	racional r3;
 	r3.numerador = (r1.numerador * r2.denominador) - (r1.denominador * r2.numerador);
 	r3.denominador = (r1.denominador * r2.denominador);
+	simplificarRacional(&r3);
 	return r3;
 }
 
-racional multiplicarRacional(racional r1, racional r2){
+racional multiplicarRacionales(racional r1, racional r2){
 	racional r3;
 	r3.numerador = r1.numerador * r2.numerador;
 	r3.denominador = r1.denominador * r2.denominador;
+	simplificarRacional(&r3);
 	return r3;
 }
 
@@ -70,22 +77,20 @@ racional dividirRacionales(racional r1, racional r2){
 	racional r3;
 	r3.numerador = r1.numerador * r2.denominador;
 	r3.denominador = r1.denominador * r2.numerador;
+	simplificarRacional(&r3);
 	return r3;
 } 
 
-void simplificarRacional(racional r3){
-	for(int i=0; i<r3.denominador; i++){
-		if(r3.numerador%i==0 && r3.denominador%i==0){
-			r3.numerador = r3.numerador/i;
-			r3.denominador = r3.denominador/i;
+void simplificarRacional(racional *r3){
+	for(int i=0; i<r3->denominador; i++){
+		if(r3->numerador%i==0 && r3->denominador%i==0){
+			r3->numerador = r3->numerador/i;
+			r3->denominador = r3->denominador/i;
 			i=1;
 		}
 	}
 }
 
 void imprimirRacional(racional r3){
-	
-	simplificarRacional(r3);
-	
-	printf("%d/%d", r3.numerador, r3.denominador);
+	printf("%d/%d \n", r3.numerador, r3.denominador);
 }
--- Chan Zurita/ADA 6/sudoku.c
+++ Chan Zurita/ADA 6/sudoku.c
@@ -7,37 +7,37 @@ typedef struct tableroMenor{
     int matrices[3][3];
 } tableroMenor;
 
-typedef struct Sudoku{
+typedef struct sudoku{
     tableroMenor tableroCompleto[3][3];
 } sudoku;
 
 //PROTOTIPOS 
-void llenarSudoku(sudoku sudokuCompleto);
-void imprimirSudoku(sudoku sudokuCompleto);
-void verificarTablero(sudoku sudokuCompleto);
+void llenarSudoku(sudoku *sudokuCompleto);
+void imprimirSudoku(sudoku *sudokuCompleto);
+void verificarTablero(sudoku *sudokuCompleto);
+
 
 //MAIN
 int main(){
+
     sudoku tablero;
-
-    time_t t;
-    srand((unsigned) time(&t));
-
-    llenarSudoku(tablero);
-    imprimirSudoku(tablero);
+    srand(time(NULL));
+    llenarSudoku(&tablero);
+    imprimirSudoku(&tablero);
+    verificarTablero(&tablero);
 
     return 0;
 }
 
 //FUNCIONES
-void llenarSudoku(sudoku sudokuCompleto){
+void llenarSudoku(sudoku *sudokuCompleto){
 
     for(int z=0; z<3; z++){
         for(int w=0; w<3; w++){
 
             for(int i=0; i<3; i++){
                 for(int j=0; j<3; j++){
-                    sudokuCompleto.tableroCompleto[z][w].matrices[i][j] = 5;
+                    (*sudokuCompleto).tableroCompleto[z][w].matrices[i][j] = rand() %9 + 1;
                 }
             } 
 
@@ -46,79 +46,139 @@ void llenarSudoku(sudoku sudokuCompleto){
     
 }
 
-void imprimirSudoku(sudoku sudokuCompleto){
+void imprimirSudoku(sudoku *sudokuCompleto){
 
     for(int z=0; z<3; z++){
         for(int w=0; w<3; w++){
 
             for(int i=0; i<3; i++){
                 for(int j=0; j<3; j++){
-                    printf("%d  ", sudokuCompleto.tableroCompleto[z][w].matrices[i][j]);
+                    printf("%d  ", (*sudokuCompleto).tableroCompleto[z][w].matrices[i][j]);
                 }
-                printf("|\n");
+                printf("    ");
             } 
 			printf("\n");
         }
+        printf("\n");
     }
 
 }
 
-void verificarTablero(sudoku sudokuCompleto){
-    int num=0;
+void verificarTablero(sudoku *sudokuCompleto){
+    int correcto=1;
+
+    while(correcto=1){
     
     //COMPROBAR MATRIZ 3X3
     for(int z=0; z<3; z++){
     	for(int w=0; w<3; w++){
-    		
-    		for(int i=0; i<3; i++){
+
+            int n = 0;
+            int matriz3x3[9];
+            //METER LOS VALORES DE LA MATRIZ DENTRO DE UN ARREGLO
+            for(int i=0; i<3; i++){
     			for(int j=0; j<3; j++){
+
+                    matriz3x3[n]=(*sudokuCompleto).tableroCompleto[z][w].matrices[i][j];
+                    n+=1;
     				
-    				for(int n=1; n<10; n++){
-    					if(sudokuCompleto.tableroCompleto[z][w].matrices[i][j] == n){
-    						num+=1;
-    					}
-    					
-    				}
+                }
     				
-    			}
     		}
+            //VERIFICAR VALORES 
+            for(int x=0; x<9; x++){
+                for(int y=0; y<9; y++){
+                    if(matriz3x3[x]==matriz3x3[y] && x!=y){
+                        correcto=0;
+                    }  
+                    if(correcto==0){
+                        break;
+                    }
+                }
+            }
+
+        }
     		
-    	}
     }
-    
-    //COMPROBAR FILA 
-    for(int i=0; i<3; i++){
-    	for(int j=0; j<3; j++){
-    		
-			for(int n=1; n<10; n++){
-    			if(sudokuCompleto.tableroCompleto[j][i].matrices[j][i] == n){
-    				num+=1;
-				}
 
+    
+    //COMPROBAR FILA
+    for(int z=0; z<3; z++){
+    	for(int w=0; w<3; w++){
+
+            int n = 0;
+            int fila[9];
+            //METER LOS VALORES DE LA FILA DENTRO DE UN ARREGLO
+            for(int i=0; i<3; i++){
+    			for(int j=0; j<3; j++){
+
+                    fila[n]=(*sudokuCompleto).tableroCompleto[z][i].matrices[w][j];
+                    n+=1;
+    				
+                }
+    				
     		}
-    	} 
-	}
-    
-    //COMPROBAR COLUMNA 
-    for(int i=0; i<3; i++){
-    	for(int j=0; j<3; j++){
-    		
-    		for(int n=1; n<10; n++){
-    			if(sudokuCompleto.tableroCompleto[i][j].matrices[i][j] == n){
-    				num+=1;
-   				}
-   			}
-    	}    
+            //VERIFICAR VALORES 
+            for(int x=0; x<9; x++){
+                for(int y=0; y<9; y++){
+                    if(fila[x]==fila[y] && x!=y){
+                        correcto=0;
+                    } 
+                    if(correcto==0){
+                        break;
+                    }
+                }
+            }
 
-	} 
-	
+        }
+    		
+    }
+
+            
+    //COMPROBAR COLUMNA 
+    for(int z=0; z<3; z++){
+    	for(int w=0; w<3; w++){
+
+            int n = 0;
+            int columna[9];
+            //METER LOS VALORES DE LA COLUMNA DENTRO DE UN ARREGLO
+            for(int i=0; i<3; i++){
+    			for(int j=0; j<3; j++){
+
+                    columna[n]=(*sudokuCompleto).tableroCompleto[w][z].matrices[j][i];
+                    n+=1;
+    				
+                }
+    				
+    		}
+            //VERIFICAR VALORES 
+            for(int x=0; x<9; x++){
+                for(int y=0; y<9; y++){
+                    if(columna[x]==columna[y] && x!=y){
+                        correcto=0;
+                    }  
+                    if(correcto==0){
+                        break;
+                    }
+                }
+            }
+
+        }
+    		
+    }
+
+    break;
+    }
+
+
 	//SALIDA
-	if(num==3){
+	if(correcto==1){
 		printf("Es un sudoku");
 	}
-	else{
-		printf("No es un sudoku");	
-	}
+	else if (correcto==0){
+        printf("No es un sudoku");
+    }
+    
 	
 }
 
